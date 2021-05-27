22d3f6f1e97117d06cfb39ba2aec9221b3e05877
Zulinlo <samm.wu2@gmail.com>
2021-05-27 21:47:41 +1000

    File layout and setup

---
+++ src/components/Calculator.js
@@ -0,0 +1,11 @@
+import './styles.scss';
+
+const Calculator = () => {
+    return (
+        <div className='container--calculator'>
+            Calc
+        </div>
+    )
+}
+
+export default Calculator;
