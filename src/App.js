a9c606690cc8322c948132f511470c6cd56f4ac3
Zulinlo <samm.wu2@gmail.com>
2021-08-20 23:01:59 +1000

    basic functionality

--- src/App.js
+++ src/App.js
@@ -1,52 +1,107 @@
+import { useState, useEffect } from "react";
+import Blink from "react-blink-text";
 import "./styles.scss";
 
-// Styling inspired by https://codepen.io/kalpeshsingh/pen/wMNpLp
-
 const App = () => {
+  const [typed, setTyped] = useState("");
+  const [operation, setOperation] = useState("");
+
+  const handleNumberInput = (input) => {
+    setTyped(`${typed}${input}`);
+  };
+
+  const handleOperationInput = (input) => {};
+
+  const handleClear = () => {
+    setTyped("");
+    setOperation("");
+  };
+
   return (
-    <div className="container">
-      <div className="calc-body">
-        <div className="calc-screen">
-          <div className="calc-operation">2536 + 419 + </div>
-          <div className="calc-typed">2955</div>
+    <section className="calculator">
+      <div className="calc-screen">
+        <div
+          className="calc-operation"
+          style={{
+            height: "28px",
+            overflow: "hidden",
+            position: "relative",
+          }}
+        >
+          <span style={{ position: "absolute", right: "0" }}>{typed}</span>
         </div>
-        <div className="calc-button-row">
-          <div className="button c">C</div>
-          <div className="button l">≠</div>
-          <div className="button l">%</div>
-          <div className="button l">/</div>
-        </div>
-        <div className="calc-button-row">
-          <div className="button">7</div>
-          <div className="button">8</div>
-          <div className="button">9</div>
-          <div className="button l">x</div>
-        </div>
-        <div className="calc-button-row">
-          <div className="button">4</div>
-          <div className="button">5</div>
-          <div className="button">6</div>
-          <div className="button l">−</div>
-        </div>
-        <div className="calc-button-row">
-          <div className="button">1</div>
-          <div className="button">2</div>
-          <div className="button">3</div>
-          <div className="button l">+</div>
-        </div>
-        <div className="calc-button-row">
-          <div className="button">.</div>
-          <div className="button">0</div>
-          <div
-            className="button"
-            style={{ padding: 0, paddingTop: 20, paddingBottom: 20 }}
-          >
-            x10<sup>x</sup>
+        <div
+          className="calc-typed"
+          style={{
+            width: "100%",
+            height: "100%",
+            overflow: "hidden",
+            position: "relative",
+          }}
+        >
+          <span style={{ position: "absolute", right: "0px" }}>{typed}</span>
+          <div style={{ position: "absolute", right: "0" }}>
+            <Blink color="#E0B612" text="_" fontSize="50px">
+              _
+            </Blink>
           </div>
-          <div className="button l">=</div>
         </div>
       </div>
-    </div>
+      <div className="calc-button-row">
+        <div className="button c" onClick={() => handleClear()}>
+          C
+        </div>
+        <div className="button l">==</div>
+        <div className="button l">%</div>
+        <div className="button l">/</div>
+      </div>
+      <div className="calc-button-row">
+        <div className="button" onClick={() => handleNumberInput(7)}>
+          7
+        </div>
+        <div className="button" onClick={() => handleNumberInput(8)}>
+          8
+        </div>
+        <div className="button" onClick={() => handleNumberInput(9)}>
+          9
+        </div>
+        <div className="button l">x</div>
+      </div>
+      <div className="calc-button-row">
+        <div className="button" onClick={() => handleNumberInput(4)}>
+          4
+        </div>
+        <div className="button" onClick={() => handleNumberInput(5)}>
+          5
+        </div>
+        <div className="button" onClick={() => handleNumberInput(6)}>
+          6
+        </div>
+        <div className="button l">−</div>
+      </div>
+      <div className="calc-button-row">
+        <div className="button" onClick={() => handleNumberInput(1)}>
+          1
+        </div>
+        <div className="button" onClick={() => handleNumberInput(2)}>
+          2
+        </div>
+        <div className="button" onClick={() => handleNumberInput(3)}>
+          3
+        </div>
+        <div className="button l">+</div>
+      </div>
+      <div className="calc-button-row">
+        <div className="button">.</div>
+        <div className="button" onClick={() => handleNumberInput(0)}>
+          0
+        </div>
+        <div className="button" style={{ padding: "20px 0 20px 0" }}>
+          Ans
+        </div>
+        <div className="button l">=</div>
+      </div>
+    </section>
   );
 };
 
